b48b6b4346ebd64f0da09d31a0e4ff24f642ce2a
FredS7 <schwabe.frederik@gmail.com>
2020-08-06 15:21:13 +0200

    Updates

---
+++ precache-manifest.17f057f9a0fd97f33fda20f94d6fc5f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928a35d4931b04d5739f88d2409b06e5",
+    "url": "/fish-tree-bird-quiz/index.html"
+  },
+  {
+    "revision": "86215deb6539bd0dc2e5",
+    "url": "/fish-tree-bird-quiz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "33451a387bfe925d207a",
+    "url": "/fish-tree-bird-quiz/static/js/2.853197b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fish-tree-bird-quiz/static/js/2.853197b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86215deb6539bd0dc2e5",
+    "url": "/fish-tree-bird-quiz/static/js/main.11e9204f.chunk.js"
+  },
+  {
+    "revision": "5e05096dcee71eb49a96",
+    "url": "/fish-tree-bird-quiz/static/js/runtime-main.70773b64.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fish-tree-bird-quiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
