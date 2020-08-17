2d98614a3d468726f4c0966ed0802543faeeb9a4
FredS7 <schwabe.frederik@gmail.com>
2020-08-17 23:54:19 +0200

    Updates

---
+++ precache-manifest.eee6a5137b29fe7c8e578d5153ce097b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53c4582271f3651717db124820059e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49e117b02602e1a80b99",
+    "url": "/static/css/main.0ba2c653.chunk.css"
+  },
+  {
+    "revision": "98e441a60ec078705a14",
+    "url": "/static/js/2.7bc6d5ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7bc6d5ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e117b02602e1a80b99",
+    "url": "/static/js/main.6e2ed8d2.chunk.js"
+  },
+  {
+    "revision": "fa383aa144ff7953351d",
+    "url": "/static/js/runtime-main.0200648d.js"
+  },
+  {
+    "revision": "8f93aab94e35e0fb2023d063dd8bae69",
+    "url": "/static/media/bird.8f93aab9.png"
+  },
+  {
+    "revision": "482e093cb231fb8df762eb294d9b2661",
+    "url": "/static/media/fish.482e093c.png"
+  },
+  {
+    "revision": "ad1ab079a97376fe157d3a5b3366300e",
+    "url": "/static/media/life.ad1ab079.png"
+  },
+  {
+    "revision": "ae020accd815166701208b99886f7fa6",
+    "url": "/static/media/lost_life.ae020acc.png"
+  },
+  {
+    "revision": "f13684d34d051239538d89198bb046c5",
+    "url": "/static/media/tree.f13684d3.png"
+  }
+]);
